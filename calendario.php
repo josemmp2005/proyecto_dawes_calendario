3b5e51cd407efa5564f5f9a6caffe83b3ac660ee
Jose <josemayenp@gmail.com>
2024-09-30 16:34:28 +0000

    update

--- calendario.php
+++ calendario.php
@@ -1,8 +1,3 @@
-<!-- 5. Dado el mes y año almacenados en variables, escribir un programa que muestre el
-calendario mensual correspondiente. Marcar el día actual en verde y los festivos
-en rojo.
-Author: José María Mayén Pérez
-Date: 28/09/2024 -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,11 +24,21 @@ Date: 28/09/2024 -->
     }
 </style>
 
-<body>
+<body>        
+    <h1>Calendario</h1>
+
     <table>
-        <h1>Calendario</h1>
+        <tr>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miércoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+            <th>Sábado</th>
+            <th>Domingo</th>
+        </tr>
         <?php
-        $mes = 9;
+        $mes = 10;
         $ano = 2024;
         $cont_dias = 1;
 
@@ -41,23 +46,40 @@ Date: 28/09/2024 -->
         $fecha->modify('first day of this month');
         $dia_semana = $fecha->format('N');
         $dias_mes = $fecha->format('t');
+        
+        $dias_festivos = [
+            '1-1',    // Año Nuevo
+            '1-6',    // Día de Reyes
+            '5-1',    // Día del Trabajador
+            '10-12',  // Día de la Hispanidad
+            '11-1',   // Día de Todos los Santos
+            '12-24',  // Nochebuena
+            '12-25',  // Navidad
+            '12-31'   // Nochevieja
+        ];
 
         for ($i = 1; $i < $dia_semana; $i++) {
             echo "<td></td>";
         }
 
         while ($cont_dias <= $dias_mes) {
-            if ($cont_dias == date("j") && $mes == date("n") && $ano == date("Y")) {
-                echo "<td style='background-color: green;'>$cont_dias</td>";
-            }else{
-                echo "<td>$cont_dias</td>";
-            }   
+            $fecha_actual = "$mes-$cont_dias";
 
+            if (in_array($fecha_actual, $dias_festivos)) {
+                echo "<td style='background-color: lightcoral;'>$cont_dias</td>";  
+            }
+            elseif ($cont_dias == date("j") && $mes == date("n") && $ano == date("Y")) {
+                echo "<td style='background-color: lightgreen;'>$cont_dias</td>"; 
+            } else {
+                echo "<td>$cont_dias</td>";
+            }
+            
             if (($dia_semana + $cont_dias - 1) % 7 == 0) {
                 echo "</tr><tr>";
             }
             $cont_dias++;
         }
+    
         for ($i = 1; $i < $dia_semana; $i++) {
             echo "<td></td>";
         }
